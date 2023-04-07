7a01373fb9b395665e4514a837cec0f91a8e23de
Johana-RHP <johana.rosicel@gmail.com>
2023-04-06 17:20:13 -0700

    combination of two different digits

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i <= 56; i++)
+	{
+		for (j = 49; j <= 57; j++)
+		{
+			if (i != j && j > i)
+			{
+				putchar(i);
+				putchar(j);
+
+				if (!(i == 56 && j == 57))
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+	putchar(10);
+
+	return (0);
+}
