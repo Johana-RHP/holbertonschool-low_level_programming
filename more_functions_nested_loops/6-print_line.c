2fc283ef1a8e9d5ec25a749cb97238277946f14e
Johana-RHP <johana.rosicel@gmail.com>
2023-04-10 22:23:39 -0700

    prints straight line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+ #include "main.h"
+
+/**
+ * print_line - prints straight line
+ * @n: number of _
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+		_putchar(10);
+	else
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar(95);
+		}
+		_putchar(10);
+	}
+}
