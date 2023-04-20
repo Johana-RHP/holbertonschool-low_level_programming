87d521dd3bfb608775595a02e604d960c97ad9fa
Johana-RHP <johana.rosicel@gmail.com>
2023-04-19 21:41:10 -0700

    prints string upside down

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string upside down
+ * @s: string
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i, n;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		;
+	}
+
+	for (n = i; n >= 0; n--)
+	{
+		_putchar(s[n]);
+	}
+
+	_putchar(10);
+}
