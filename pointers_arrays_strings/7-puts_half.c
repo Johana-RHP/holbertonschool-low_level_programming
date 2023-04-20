b2605964b14415443ecc448912fa90bd919ae04a
Johana-RHP <johana.rosicel@gmail.com>
2023-04-19 23:54:48 -0700

    prints the second half of the string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * _strlen - calculates the length of the string
+ * @s: string
+ *
+ * Return: chain length
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		;
+	}
+
+	return (i);
+}
+
+/**
+ * puts_half - prints the second half of the string
+ * @str: string
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i, half, len;
+
+	len = _strlen(str);
+
+	if (len % 2 == 0)
+		half = len / 2;
+	else
+		half = (len - 1) / 2;
+
+	for (i = half; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar(10);
+}
