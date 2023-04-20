e20c97db3448bc7fbe23e4b0cb2b55f0390ff8d9
Johana-RHP <johana.rosicel@gmail.com>
2023-04-19 21:23:00 -0700

    calculates the length of the string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - calculates the length of the string
+ * @s: string
+ *
+ * Return: chain length
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		;
+	}
+
+	return (i);
+}
