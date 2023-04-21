2dc7ff3e862273073058c738de1df6fd9ad0b9f2
Johana-RHP <johana.rosicel@gmail.com>
2023-04-20 18:35:53 -0700

    correct condition in for

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -33,10 +33,16 @@ void puts_half(char *str)
 	len = _strlen(str);
 
 	if (len % 2 == 0)
+	{
 		half = len / 2;
+		for (i = half; i < len; i++)
+                	_putchar(str[i]);
+	}
 	else
+	{
 		half = (len - 1) / 2;
-	for (i = half; i < len; i++)
-		_putchar(str[i]);
+		for (i = (half + 1); i < len; i++)
+			_putchar(str[i]);
+	}
 	_putchar(10);
 }
