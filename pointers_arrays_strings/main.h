24a43677fddc1bf737681faed91b157580451613
Johana-RHP <johana.rosicel@gmail.com>
2023-05-05 21:17:29 -0700

    add statements to main

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -13,5 +13,25 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *n);
+char *_memset(char *s, char b, unsigned int n);
+void set_string(char **s, char *to);
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 
-#endif /*HEADER_FILE*/
+#endif /*MAIN_H*/
