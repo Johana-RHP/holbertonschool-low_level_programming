65953488832cc8454a742f7b1ed48500be9868d5
Johana-RHP <johana.rosicel@gmail.com>
2023-05-05 21:36:09 -0700

    tasks

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#define NULL '\0'
+/**
+ * _strchr - Locates a character in a string
+ * @s: The string to check
+ * @c: The character to locate
+ * Return: A pointer to the 1st occurrence of the char c in the string s,
+ * or NULL if the character is not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	if (s[i] == c)
+	{
+		return (&s[i]);
+	}
+	return (NULL);
+}
